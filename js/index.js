0a4e3cbec2c22cee3ed1714435b70216e9178841
anaspen62 <nastenapenzeva123@yandex.ru>
2023-08-04 01:35:55 +0300

    init project

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+
+container.innerHTML = `
+    <h1>Привет</h1>
+    <span>Это тестовый текст</span>
+    <form>
+        <input type="text">
+    </form>
+`;
